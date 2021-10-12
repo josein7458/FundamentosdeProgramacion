8c8a9c9a17a6121c1c6322b7a2fb8f19ac2d2ed4
josein7458 <92315586+josein7458@users.noreply.github.com>
2021-10-12 11:17:41 -0500

    Archivo de ejemplo
    Es un archivo que el profesor me pidio que lo subiera para mostrar.

---
+++ guevara cunalata josein (2).cpp
@@ -0,0 +1,20 @@
+/*3: Construya un programa que dado como dato la calificación de un alumno en un examen,
+escriba Aprobado si su calificación es mayor o igual a 8 y reprobado en caso contrario*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n1,aprobado,reprobado,num;
+	cout<<"nota del examen "<<endl;
+	cin>>num;
+	
+	if (n1>=8){
+		cout<<"usted esta aprobado "<<endl;
+	}
+	else
+	{
+		cout<<"usted a reprobado "<<endl;
+	}
+    return 0;
+}
