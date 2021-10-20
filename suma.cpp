8920c9d9152ff832c608d7104c052a9ed024e12f
ANABELLA6 <92494372+ANABELLA6@users.noreply.github.com>
2021-10-20 10:40:50 -0500

    JOMARA LARA
    CREACIÓN DE DIRECTORIOS Y ARCHIVOS EN VIM

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float x,y,z;
+        cout<<"Ingresar dos números  x y:",
+	cin>>x>>y;
+	z=x+y;
+        cout<<"El resultado es:";
+	cout<<z<<endl;
+	return 0;
+}
