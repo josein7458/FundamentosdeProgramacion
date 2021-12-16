2f0a8ef735e97c7ae7ab4e146e1da24692201c39
ANABELLA6 <jomara.lara.vera@utelvt.edu.ec>
2021-12-16 17:59:44 -0500

    archivo c++

---
+++ ActividadC2/LaraJomara/sumasimple.cpp
@@ -0,0 +1,14 @@
+ //suma simple de dos números//
+#include<iostream>
+using namespace std;
+int main()
+  {
+	  float x,y,z;
+	  cout<<"Ingrese el primer número :";
+	  cin>>x; 
+	  cout<<"Ingrese el segundo número :";
+	  cin>>y;
+	  z=x+y; 
+	  cout<<"El resultado es:"<<z<<endl;
+	  return 0;
+  }
