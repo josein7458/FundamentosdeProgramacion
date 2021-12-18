590694737e899016d8941c74f00703b91a423dd3
Mayerli Andrade Mera <92406583+Mayerli24@users.noreply.github.com>
2021-12-18 14:40:41 -0500

    Add files via upload

---
+++ ActividadC2/AndradeMayerli/SUMA SIMPLE DE 2 NÚMEROS.cpp
@@ -0,0 +1,15 @@
+/********  SUMA SIMPLE DE DOS NÚMEROS  *******/
+#include<iostream>
+using namespace std;
+int main()
+{
+	float A, B, C;
+	cout<<"Ingrese el primer número A :";
+	cin>>A;
+	cout<<"Ingrese el segundo número B :";
+	cin>>B;
+	C=A+B;
+	cout<<"El resultado de la suma es:"<<C<<endl;
+	
+	return 0;
+}
